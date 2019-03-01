cd8750782803364a47f3eaa5dc20a4107bf8213a
Greg A <gautric@redhat.com>
2019-03-01 20:33:11 +0100

    Add PatternFly Card

--- web-vuejs/src/components/Map.vue
+++ web-vuejs/src/components/Map.vue
@@ -1,12 +1,84 @@
 <template lang="html">
 
   <section class="map">
-    <div id="mapid"></div>
+    <div class="container-fluid">
 
-    <div>
-      <pre>lat {{lattitude}}</pre>
-      <pre>long {{longitude}}</pre>
-      <pre>zoom {{zoom}}</pre>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Map</h3>
+        </div>
+        <div class="panel-body" id="mapid">
+        </div>
+      </div>
+
+      <div class="container-fluid container-cards-pf">
+        <div class="row row-cards-pf">
+          <div class="col-xs-6 col-sm-4 col-md-4">
+            <div class="card-pf card-pf-accented card-pf-aggregate-status">
+              <h2 class="card-pf-title">
+                <span class="mdi mdi-twitter"></span> Tweet
+              </h2>
+              <div class="card-pf-body">
+                {{tweet}}
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-4 col-md-4">
+            <div class="card-pf card-pf-accented card-pf-aggregate-status">
+              <h2 class="card-pf-title">
+                 <span class="mdi mdi-arrow-up-down-bold-outline"></span> Lattitude
+              </h2>
+              <div class="card-pf-body">
+                {{lattitude}}
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-4 col-md-4">
+            <div class="card-pf card-pf-accented card-pf-aggregate-status">
+              <h2 class="card-pf-title">
+                <span class="mdi mdi-arrow-left-right-bold-outline"></span> Longitude
+              </h2>
+              <div class="card-pf-body">
+                {{longitude}}
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-4 col-md-4">
+            <div class="card-pf card-pf-accented card-pf-aggregate-status">
+              <h2 class="card-pf-title">
+                <span class="mdi mdi-clock-outline"></span> Time
+              </h2>
+              <div class="card-pf-body">
+                {{time}}
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-4 col-md-4">
+            <div class="card-pf card-pf-accented card-pf-aggregate-status">
+              <h2 class="card-pf-title">
+                <span class="mdi mdi-magnify-plus-outline"></span> Zoom
+              </h2>
+              <div class="card-pf-body">
+                {{zoom}}
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-4 col-md-4">
+            <div class="card-pf card-pf-accented card-pf-aggregate-status">
+              <h2 class="card-pf-title">
+                <span class="mdi mdi-counter"></span> Counter
+              </h2>
+              <div class="card-pf-body">
+                {{counter}}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      </div>
     </div>
   </section>
 
@@ -29,14 +101,19 @@
       },
     },
     mounted() {
+
+  //    this.$options.sockets.onmessage = (event) => console.log(event);
+
+      this.$options.sockets.onmessage = this.onmessage;
+
+
       this._leaflet = L.map('mapid').setView([this.lat, this.long], this.zoom);
       console.log(process.env);
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Yodel / Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
-      //  accessToken: 'pk.eyJ1IjoiZ3JlZ2lvIiwiYSI6ImNqc2x5d3J3dTFmYjk0NGxpcnhzcTNzbGMifQ.dcEOjBDtjMlxSkl4HYghvA'
-       accessToken: process.env.VUE_APP_MAPBOX_ACCESS_TOKEN
+        accessToken: process.env.VUE_APP_MAPBOX_ACCESS_TOKEN
 
     }).addTo(this._leaflet);
 
@@ -49,7 +126,10 @@
           _leaflet: undefined,
           longitude: this.long,
           lattitude: this.lat,
-          zoom: 12
+          zoom: 12,
+          tweet:'',
+          time:'',
+          counter: 0
         }
     },
     methods: {
@@ -57,11 +137,25 @@
         this.longitude = this._leaflet.getCenter().lng;
         this.lattitude = this._leaflet.getCenter().lat;
         this.zoom = this._leaflet.getZoom();
+      },
+      onmessage: function(event){
+        var tweet = JSON.parse(event.data);
+        console.log(tweet);
+        if(tweet.tweet.coordinates.type==='Point') {
+          this._leaflet.panTo(new L.LatLng(tweet.tweet.coordinates.coordinates[1], tweet.tweet.coordinates.coordinates[0]));
+          this.tweet = tweet.payload;
+          this.time = tweet.tweet.created_at;
+          this.counter = this.counter+1;
+        }
+
       }
     },
     computed: {
     }
 }
+
+
+
 </script>
 
 <style scoped lang="scss">
@@ -71,6 +165,6 @@
     margin-right: auto
   }
 
-  #mapid { width: 80%; height: 450px; }
+  #mapid { height: 550px; }
 
 </style>
--- web-vuejs/src/main.js
+++ web-vuejs/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueNativeSock from 'vue-native-websocket'
 
 Vue.config.productionTip = false
 
-new Vue({
+Vue.use(VueNativeSock, 'ws://localhost:1880/ws/tweet',
+{
+  format: 'json',
+  reconnection: true, // (Boolean) whether to reconnect automatically (false)
+  reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
+  reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
+})
+
+const vm = new Vue({
   render: h => h(App)
 }).$mount('#app')
