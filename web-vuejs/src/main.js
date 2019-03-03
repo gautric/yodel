6de4fe542439f56ffba39bab80b0d832ec77c392
Greg A <gautric@redhat.com>
2019-03-03 13:19:15 +0100

    formatting

--- web-vuejs/src/main.js
+++ web-vuejs/src/main.js
@@ -4,13 +4,12 @@ import VueNativeSock from 'vue-native-websocket'
 
 Vue.config.productionTip = false
 
-Vue.use(VueNativeSock, 'ws://localhost:1880/api/ws/generic',
-{
-  format: 'json',
-  reconnection: true, // (Boolean) whether to reconnect automatically (false)
-  reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
-  reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
-})
+Vue.use(VueNativeSock, 'ws://localhost:1880/api/ws/generic', {
+    format: 'json',
+    reconnection: true, // (Boolean) whether to reconnect automatically (false)
+    reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
+    reconnectionDelay: 3000 // (Number) how long to initially wait before attempting a new (1000)
+  })
 
 const vm = new Vue({
   render: h => h(App)
