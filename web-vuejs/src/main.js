4fdb5e5cf7668a6051ff1225fa7cc9a3b2d3060c
gautric <gautric@redhat.com>
2019-03-05 15:54:25 +0100

    formating

--- web-vuejs/src/main.js
+++ web-vuejs/src/main.js
@@ -5,18 +5,18 @@ import VueNativeSock from 'vue-native-websocket'
 import Map from './components/Map.vue'
 import Card from './components/Card.vue'
 
-Vue.component('Map', Map);
-Vue.component('Card', Card);
+Vue.component('Map', Map)
+Vue.component('Card', Card)
 
 Vue.config.productionTip = false
 
 Vue.use(VueNativeSock, 'ws://localhost:1880/api/ws/generic', {
-    format: 'json',
-    reconnection: true, // (Boolean) whether to reconnect automatically (false)
-    reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
-    reconnectionDelay: 3000 // (Number) how long to initially wait before attempting a new (1000)
-  })
+  format: 'json',
+  reconnection: true, // (Boolean) whether to reconnect automatically (false)
+  reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
+  reconnectionDelay: 3000 // (Number) how long to initially wait before attempting a new (1000)
+})
 
-const vm = new Vue({
+new Vue({
   render: h => h(App)
 }).$mount('#app')
