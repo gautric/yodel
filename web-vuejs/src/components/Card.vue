56243c6d9c3778d4197242bf9e87dea6f0263484
gautric <gautric@redhat.com>
2019-03-05 15:38:54 +0100

    Add Card component

---
+++ web-vuejs/src/components/Card.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+
+  <section class="card">
+    <div class="col-xs-6 col-sm-4 col-md-4">
+      <div class="card-pf card-pf-accented card-pf-aggregate-status">
+        <h2 class="card-pf-title">
+          <span  class="mdi" :class="icon"/> {{title}}
+        </h2>
+        <div class="card-pf-body">
+          {{message}}
+        </div>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'Card',
+    props: [
+      'icon',
+      'title',
+      'message'
+    ],
+}
+</script>
+
+<style scoped lang="scss">
+  .card {
+
+  }
+</style>
