475e3fec212dd8e99f28c375f6d9c9c34dd5500e
gautric <gautric@redhat.com>
2019-03-06 14:08:32 +0100

    use geojson and code formatting

--- web-vuejs/src/components/Map.vue
+++ web-vuejs/src/components/Map.vue
@@ -19,7 +19,7 @@
           <Card title="Time" icon="mdi-clock-outline" :text="info.created_at"/>
           <Card title="Zoom" icon="mdi-magnify-plus-outline" :text="zoom"/>
           <Card title="Counter" icon="mdi-counter" :text="counter"/>
-          <Card title="Lan" icon="mdi-flag" :text="info.lang"/>
+          <Card title="Lang" icon="mdi-flag" :text="info.lang"/>
 
         </div>
       </div>
@@ -30,91 +30,101 @@
 </template>
 
 <script lang="js">
+import L from 'leaflet'
 
-  import L from 'leaflet'
-
-  export default  {
-    name: 'Map',
-    props: {
-      long:{
-        type: String,
-        required: true
-      },
-      lat:{
-        type: String,
-        required: true
-      },
+export default {
+  name: 'Map',
+  props: {
+    long: {
+      type: String,
+      required: true
     },
-
-    mounted() {
-
-  //    this.$options.sockets.onmessage = (event) => console.log(event);
-
-      this.$options.sockets.onmessage = this.onmessage;
-
-      this._leaflet = L.map('mapid').setView([this.lat, this.long], this.zoom);
-      console.log(process.env);
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Yodel / Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: process.env.VUE_APP_MAPBOX_ACCESS_TOKEN
-
-    }).addTo(this._leaflet);
-
-      this._leaflet.on("moveend", this.move);
-      this._leaflet.on("move", this.move);
-
+    lat: {
+      type: String,
+      required: true
     },
-    data: function () {
-        return {
-          _leaflet: undefined,
-          longitude: this.long,
-          lattitude: this.lat,
-          zoom: 12,
-          counter: 0,
-          info: {
-            "payload": "",
-            "lang": "",
-            "created_at": ""
-          },
-          _list:[],
-        }
+  },
+  mounted() {
+    this.$options.sockets.onmessage = this.onmessage
+
+    this._leaflet = L.map('mapid').setView([this.lat, this.long], this.zoom)
+    console.log(process.env)
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Yodel / Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: process.env.VUE_APP_MAPBOX_ACCESS_TOKEN
+
+    }).addTo(this._leaflet)
+
+    this._leaflet.on('moveend', this.move)
+    this._leaflet.on('move', this.move)
+
+  },
+  data: function () {
+    return {
+      _leaflet: undefined,
+      longitude: this.long,
+      lattitude: this.lat,
+      zoom: 12,
+      counter: 0,
+      info: {
+        'payload': '',
+        'lang': '',
+        'created_at': ''
+      }
+    }
+  },
+  methods: {
+    move: function (e) {
+      this.longitude = this._leaflet.getCenter().lng
+      this.lattitude = this._leaflet.getCenter().lat
+      this.zoom = this._leaflet.getZoom()
     },
-    methods: {
-      move: function(e){
-        this.longitude = this._leaflet.getCenter().lng;
-        this.lattitude = this._leaflet.getCenter().lat;
-        this.zoom = this._leaflet.getZoom();
-      },
-      onmessage: function(event){
-        var data = JSON.parse(event.data);
-        if(data.coordinates.type==='Point') {
-
-          var position = new L.LatLng(data.coordinates.coordinates[1], data.coordinates.coordinates[0]);
-          var marker = new L.Marker(position);
-          this._leaflet.panTo(position);
-          this._leaflet.addLayer(marker);
-
-          this.info = data;
-          this.longitude = data.coordinates.coordinates[1];
-          this.lattitude = data.coordinates.coordinates[0];
-          this.counter = this.counter + 1;
-        }
+    onEachFeature: function (feature, layer) {
+      // does this feature have a property named popupContent?
+      if (feature.properties && feature.properties.text) {
+        layer.bindPopup(feature.properties.text)
       }
     },
-    computed: {
+    onmessage: function (event) {
+      var data = JSON.parse(event.data)
+      if (data.coordinates.type === 'Point') {
+        var position = new L.LatLng(data.coordinates.coordinates[1], data.coordinates.coordinates[0])
+
+        this._leaflet.panTo(position)
+
+        var geoJSON = {
+          'type': 'Feature',
+          'properties': {
+            'text': data.payload
+          },
+          'geometry': data.coordinates
+        }
+
+        L.geoJSON(geoJSON, {
+          onEachFeature: this.onEachFeature
+        }).addTo(this._leaflet)
+
+        this.info = data
+        this.longitude = data.coordinates.coordinates[1]
+        this.lattitude = data.coordinates.coordinates[0]
+        this.counter = this.counter + 1
+      }
     }
+  },
+  computed: {}
 }
 </script>
 
 <style scoped lang="scss">
-  .map {
+.map {
     display: block;
     margin-left: auto;
-    margin-right: auto
-  }
+    margin-right: auto;
+}
 
-  #mapid { height: 560px; }
-
+#mapid {
+    height: 560px;
+}
 </style>
